4c46c23c48e62f6ada69d5cf2a7dfb85272b455e
KIMKICHAN <rlcks980705@gmail.com>
2023-05-31 15:39:29 +0900

    Time: 32 ms (29.07%), Space: 16.1 MB (55.69%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -11,7 +11,16 @@ class Solution(object):
         :rtype: int
         """
         
-        if not root:
-            return 0
+        stack=[[root,1]]
+        res=0
         
-        return 1+ max(self.maxDepth(root.left), self.maxDepth(root.right))
+        while stack:
+            
+            node,depth=stack.pop()
+            
+            if node:
+                
+                res=max(res,depth)
+                stack.append([node.left,depth+1])
+                stack.append([node.right,depth+1])
+        return res
