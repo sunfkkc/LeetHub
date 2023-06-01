8bded6f8cb79bbb2d1efb0aa10155b8ed814c22a
KIMKICHAN <rlcks980705@gmail.com>
2023-06-01 16:36:18 +0900

    Time: 1230 ms (5.26%), Space: 21.4 MB (35.53%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -4,39 +4,40 @@ class Solution(object):
         :type graph: List[List[int]]
         :rtype: List[int]
         """
-        
+
         n=len(graph)
         
-        indeg=[0]*n
+        indegree=[0]*n
         
-        adj=[[] for i in range(n)]
+        adj=defaultdict(list)
         
         for i in range(n):
             
-            indeg[i]=len(graph[i])
-            
+            indegree[i]=len(graph[i])
             for j in graph[i]:
+                
                 adj[j].append(i)
                 
+            
+        print(adj)
+        print(indegree)
+        
         q=deque()
+        
         res=[]
         
-        print(indeg)
-        print(adj)
-        
         for i in range(n):
-            if indeg[i]==0:
+            if indegree[i]==0:
                 q.append(i)
-        
+                
         while q:
-            
             u=q.popleft()
             
             res.append(u)
             
             for i in adj[u]:
-                indeg[i]-=1
-                if indeg[i]==0:
+                indegree[i]-=1
+                
+                if indegree[i]==0:
                     q.append(i)
-                    
         return sorted(res)
