7c3516ebf6d68679fdb44341b37aaf5973b5cf7c
KIMKICHAN <rlcks980705@gmail.com>
2023-06-05 18:01:28 +0900

    Time: 1522 ms (5.15%), Space: 13.8 MB (97.43%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        
+        visited=[0]
+        
+        
+        stack=[rooms[0]]
+        
+        
+        while stack:
+            print(stack)    
+            keys=stack.pop()
+            print(keys)
+            
+            
+            for key in keys:
+                
+                if key not in visited:
+                    
+                    visited.append(key)
+                    stack.append(rooms[key])
+                    
+        return True if len(visited)==len(rooms) else False
+        
