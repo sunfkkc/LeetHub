8404678245ca1dea93f021c1254bbe135e1e5a90
KIMKICHAN <rlcks980705@gmail.com>
2023-06-06 17:16:22 +0900

    Time: 26 ms (50.26%), Space: 13.5 MB (49.21%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        
+        
+        res=[]
+        
+        def bt(i, dots, ip):
+            
+            if dots==4 and i==len(s):
+                res.append(ip[:-1])
+                return
+            
+            if dots>4:
+                return
+            
+            for j in range(i, min(i+3,len(s)) ):
+                if int(s[i:j+1]) <=255 and (i==j or s[i] != "0"):
+                    bt(j+1,dots+1, ip+s[i:j+1]+".")
+        
+        bt(0,0,"")
+        return res
