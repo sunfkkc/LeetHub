e3b726710c025cfeea5c46d8c5d64d7a4e074e85
KIMKICHAN <rlcks980705@gmail.com>
2023-06-06 14:50:58 +0900

    Time: 294 ms (35.46%), Space: 35.4 MB (5.48%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,33 @@
 class Solution(object):
         
-        
-    
     def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        row,col = len(grid), len(grid[0])
         
-        answer=0
+        visited=set()
         
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c]=="1":
-                    self.dfs(grid,r,c)
-                    answer+=1
-        return answer
+        res=0
         
-    def dfs(self,grid,r,c):
         
-        if( r<0 or c<0 or r>=len(grid) or c>= len(grid[0])): return
-        
-        if( grid[r][c] =="1"):
-            grid[r][c]="0"
-            self.dfs(grid,r+1,c)
-            self.dfs(grid,r-1,c)
-            self.dfs(grid,r,c-1)
-            self.dfs(grid,r,c+1)
-        
+        def dfs(r,c):
+            
+            if r>=0 and r<=len(grid)-1 and c>=0 and c<=len(grid[0])-1 and grid[r][c]=="1" and (r,c) not in visited:
+                visited.add((r,c))
+                dfs(r-1,c)
+                dfs(r+1,c)
+                dfs(r,c-1)
+                dfs(r,c+1)
+            
+                
+                
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if (r,c) not in visited and grid[r][c]=="1":
                     
-                    
-                    
+                    dfs(r,c)
+                    res+=1
+        
+        return res
+                
