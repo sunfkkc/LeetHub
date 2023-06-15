82cffe2dbf4d0badebc75a9a630b0c155c414362
KIMKICHAN <rlcks980705@gmail.com>
2023-06-15 13:36:42 +0900

    Time: 644 ms (16.47%), Space: 35.3 MB (5.49%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -4,31 +4,36 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
+        
+        
         res=[]
         part=[]
         
-        def cal(w,l,r):
+        def isPal(w):
+            n=len(w)
+            
+            l,r=0,n-1
+            
             while l<r:
+                
                 if w[l] != w[r]:
                     return False
                 l,r=l+1,r-1
             return True
-            
         
         def bt(i):
+            
             
             if i==len(s):
                 res.append(list(part))
                 return
             
-            for j in range(i,len(s)):
-                if cal(s,i,j):
+            for j in range(i,len(s)+1):
+                
+                if isPal(s[i:j+1]):
                     part.append(s[i:j+1])
                     bt(j+1)
                     part.pop()
-                    
+        
         bt(0)
         return res
-        
-        
-        
