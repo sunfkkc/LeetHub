4e01afc0afe3e91a7866adc626bf47ff81349449
KIMKICHAN <rlcks980705@gmail.com>
2023-06-19 22:46:17 +0900

    Time: 114 ms (58.36%), Space: 13.5 MB (95.22%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -6,25 +6,40 @@ class Solution(object):
         """
         
         
-        visited=set()
-        def bfs(r,c):
+        def bfs(i,j):
+            q=deque()
+            q.append((i,j))
+            area=0
             
-            if r<0 or r>=len(grid) or c<0 or c>=len(grid[0]) or (r,c) in visited or grid[r][c] != 1:
-                return 0
-            
-            
-            
-            visited.add((r,c))
+            while q:
+                r,c=q.popleft()
+                grid[r][c]=0
+                area+=1
                 
+                if r>0 and grid[r-1][c]==1:
+                    grid[r - 1][c] = 0
+                    q.append((r-1,c))
+                
+                if r<len(grid)-1 and grid[r+1][c]==1:
+                    grid[r + 1][c] = 0
+                    q.append((r+1,c))
+                    
+                if c>0 and grid[r][c-1]==1:
+                    grid[r][c - 1] = 0
+                    q.append((r,c-1))
+                    
+                if c<len(grid[0])-1 and grid[r][c+1]==1:
+                    grid[r][c + 1] = 0
+                    q.append((r,c+1))
             
-            return (1+bfs(r-1,c)+
-            bfs(r+1,c)+
-            bfs(r,c-1)+
-            bfs(r,c+1))
-            
+            print(i,j,area)
+            return area
+        
         res=0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                res=max( bfs(i,j),res)
-            
+                
+                if grid[i][j]==1:
+                    res=max(bfs(i,j),res)
+                    
         return res
