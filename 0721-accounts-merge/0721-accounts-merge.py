1c6e3f0f4cbc5bfd198aae17a485e62e46706e3b
KIMKICHAN <rlcks980705@gmail.com>
2023-06-23 15:18:32 +0900

    Time: 218 ms (29.95%), Space: 19.5 MB (63.29%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def accountsMerge(self, accounts):
+        """
+        :type accounts: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        
+        
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+
+        def union(x, y):
+            parent[find(x)] = find(y)
+
+        email_to_name = {}
+        parent = {}
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                if email not in parent:
+                    parent[email] = email
+                email_to_name[email] = name
+                union(account[1], email)
+
+        unions = defaultdict(list)
+        for email in parent.keys():
+            unions[find(email)].append(email)
+
+        return [[email_to_name[email]] + sorted(emails) for email, emails in unions.items()]
