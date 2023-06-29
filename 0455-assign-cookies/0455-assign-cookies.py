e7b64cfcece8e334997d5f86a488f38dea563f29
KIMKICHAN <rlcks980705@gmail.com>
2023-06-30 00:00:12 +0900

    Time: 3121 ms (5.05%), Space: 14.7 MB (85.16%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        
+        res=0
+        g.sort()
+        s.sort()
+        while g and s:
+            
+            a=g.pop()
+            
+            for k in s:
+                if k>=a:
+                    res+=1
+                    s.remove(k)
+                    break
+        return res
