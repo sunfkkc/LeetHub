1edb1593ac12efe079eb03b294dcaf7cadbbe1f8
KIMKICHAN <rlcks980705@gmail.com>
2023-06-29 23:04:11 +0900

    Time: 681 ms (10.98%), Space: 20.9 MB (12.11%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -4,35 +4,38 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        
         visited=set()
-        q=deque()
+        
+        self.res=0
+        
+        def dfs(i,j):
+            
+            for ni,nj in [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]:
+                
+                if 0<=ni<=len(grid)-1 and 0<=nj<=len(grid[0])-1:
+                    
+                    if grid[ni][nj]==1:
+                        
+                        if (ni,nj) not in visited:
+                            visited.add((ni,nj))
+                            dfs(ni,nj)
+                            
+                    else:
+                        self.res+=1
+                else:
+                    self.res+=1
         
         for i in range(len(grid)):
             a=False
             for j in range(len(grid[0])):
                 if grid[i][j]==1:
-                    q.append((i,j))
+                    visited.add((i,j))
+                    dfs(i,j)
                     a=True
                     break
             if a:
                 break
-        res=0
-        while q:
-            i,j=q.popleft()
-            
-            if (i,j) in visited:
-                continue
-            
-            visited.add((i,j))
-            
-            for ni,nj in [[i-1,j],[i+1,j],[i,j-1],[i,j+1]]:
-                if 0<=ni<=len(grid)-1 and 0<=nj<=len(grid[0])-1:
-                    
-                    if grid[ni][nj]==1:
-                        q.append((ni,nj))
-                    else:
-                        res+=1
-                else:
-                    res+=1
-                        
-        return res  
+                
+        
+        return self.res
