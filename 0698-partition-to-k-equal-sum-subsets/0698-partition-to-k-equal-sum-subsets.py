6ecb79e0fffd14db2d8249857d8a2cf532993d1e
KIMKICHAN <rlcks980705@gmail.com>
2023-07-06 13:39:38 +0900

    Time: 54 ms (96.08%), Space: 13.4 MB (72.55%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,35 +1,28 @@
-class Solution(object):
+class Solution:
     def canPartitionKSubsets(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        
-        g=sum(nums)//k
-        memo={}
+        n = len(nums)
 
-        if g != sum(nums)/k:
-            return False
-        
-        nums.sort(reverse=True)
-        
-        a=[0]*k
-        
-        def bt(i):
-            
-            if i==len(nums):
-                return True
-            if tuple(a) in memo:
-                return memo[tuple(a)]
+        if sum(nums)%k != 0: return False
+
+        target = sum(nums)//k
+
+        nums.sort(reverse = True)
+
+        def backtrack(i,ans):
+            if i == n:
+                return len(set(ans)) == 1
+
             for j in range(k):
-                
-                if a[j] + nums[i] <= g:
-                    a[j]+=nums[i]
-                    if bt(i+1):
+                if ans[j] + nums[i] <= target:
+                    ans[j] += nums[i]
+
+                    if backtrack(i+1,ans):
                         return True
-                    
-                    a[j]-=nums[i]
-            memo[tuple(a)] = False
-            return memo[tuple(a)]
-        return bt(0)
+
+                    ans[j] -= nums[i]
+
+                    if not ans[j]: break
+
+            return False
+
+        return backtrack(0,[0]*k)
